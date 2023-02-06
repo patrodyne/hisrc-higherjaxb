cb933ed0d1b5751cb0adb85b76a51767ed3ab962
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2023-02-06 14:19:22 -0500

    Build using JDK 17 and source/target Java 11.

--- plugin-core/src/main/java/org/jvnet/higherjaxb/mojo/resolver/tools/ReResolvingEntityResolverWrapper.java
+++ plugin-core/src/main/java/org/jvnet/higherjaxb/mojo/resolver/tools/ReResolvingEntityResolverWrapper.java
@@ -1,44 +1,50 @@
 package org.jvnet.higherjaxb.mojo.resolver.tools;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-public class ReResolvingEntityResolverWrapper implements EntityResolver {
-
+public class ReResolvingEntityResolverWrapper implements EntityResolver
+{
+	private Logger logger = LoggerFactory.getLogger(ReResolvingEntityResolverWrapper.class);
+	public Logger getLogger() { return logger; }
+	
 	private final EntityResolver entityResolver;
 
-	public ReResolvingEntityResolverWrapper(EntityResolver entityResolver) {
-		if (entityResolver == null) {
-			throw new IllegalArgumentException(
-					"Provided entity resolver must not be null.");
-		}
+	public ReResolvingEntityResolverWrapper(EntityResolver entityResolver)
+	{
+		if (entityResolver == null)
+			throw new IllegalArgumentException("Provided entity resolver must not be null.");
+
 		this.entityResolver = entityResolver;
 	}
 
 	@Override
 	public InputSource resolveEntity(String publicId, String systemId)
-			throws SAXException, IOException {
-		// System.out.println(MessageFormat.format("Resolving publicId [{0}], systemId [{1}].",
-		// publicId, systemId));
-		final InputSource resolvedInputSource = this.entityResolver
-				.resolveEntity(publicId, systemId);
-		if (resolvedInputSource == null) {
-			// System.out.println("Resolution result is null.");
+		throws SAXException, IOException
+	{
+		getLogger().debug(MessageFormat.format("Resolving publicId [{0}], systemId [{1}].",
+			publicId, systemId));
+		
+		final InputSource resolvedInputSource = this.entityResolver.resolveEntity(publicId, systemId);
+		if (resolvedInputSource == null)
+		{
+			getLogger().debug("Resolution result is null.");
 			return null;
-		} else {
-			// System.out.println(MessageFormat.format(
-			// "Resolved to publicId [{0}], systemId [{1}].",
-			// resolvedInputSource.getPublicId(),
-			// resolvedInputSource.getSystemId()));
-			final String pId = publicId != null ? publicId
-					: resolvedInputSource.getPublicId();
-			final String sId = systemId != null ? systemId
-					: resolvedInputSource.getSystemId();
-			return new ReResolvingInputSourceWrapper(this.entityResolver,
-					resolvedInputSource, pId, sId);
+		}
+		else
+		{
+			getLogger().debug(MessageFormat.format("Resolved to publicId [{0}], systemId [{1}].",
+				resolvedInputSource.getPublicId(), resolvedInputSource.getSystemId()));
+			
+			final String pId = publicId != null ? publicId : resolvedInputSource.getPublicId();
+			final String sId = systemId != null ? systemId : resolvedInputSource.getSystemId();
+			return new ReResolvingInputSourceWrapper(this.entityResolver, resolvedInputSource, pId, sId);
 		}
 	}
 }
--- plugin-core/src/test/java/org/jvnet/higherjaxb/mojo/net/tests/URILastModifiedResolverTest.java
+++ plugin-core/src/test/java/org/jvnet/higherjaxb/mojo/net/tests/URILastModifiedResolverTest.java
@@ -12,30 +12,37 @@ import org.junit.jupiter.api.Test;
 import org.jvnet.higherjaxb.mojo.net.CompositeURILastModifiedResolver;
 import org.jvnet.higherjaxb.mojo.net.JarURILastModifiedResolver;
 import org.jvnet.higherjaxb.mojo.net.URILastModifiedResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class URILastModifiedResolverTest {
-
+public class URILastModifiedResolverTest
+{
+	private Logger logger = LoggerFactory.getLogger(URILastModifiedResolverTest.class);
+	public Logger getLogger() { return logger; }
+	
 	@Test
-	public void getsFileURIFromJarFileURICorrectly() throws URISyntaxException,
-			MalformedURLException, IOException {
+	public void getsFileURIFromJarFileURICorrectly()
+		throws URISyntaxException, MalformedURLException, IOException
+	{
 		final URI jarURI = Test.class.getResource("Test.class").toURI();
 		final String jarURIString = jarURI.toString();
-		System.out.println(jarURIString);
+		getLogger().debug(jarURIString);
+		
 		final String partJarURIString = jarURIString.substring(0,
-				jarURIString.indexOf(JarURILastModifiedResolver.SEPARATOR));
+			jarURIString.indexOf(JarURILastModifiedResolver.SEPARATOR));
 		final URI partJarURI = new URI(partJarURIString);
-		final URILastModifiedResolver resolver = new CompositeURILastModifiedResolver(
-				new SystemStreamLog());
-		final URI fileURI = getClass().getResource(
-				getClass().getSimpleName() + ".class").toURI();
-
+		final URILastModifiedResolver resolver =
+			new CompositeURILastModifiedResolver(new SystemStreamLog());
+		final URI fileURI = getClass().getResource(getClass().getSimpleName() + ".class").toURI();
+		
 		assertNotNull(resolver.getLastModified(jarURI));
 		assertNotNull(resolver.getLastModified(partJarURI));
 		assertNotNull(resolver.getLastModified(fileURI));
-
+		
 		// Switch to true to tests HTTP/HTTPs
 		boolean online = false;
-		if (online) {
+		if (online)
+		{
 			final URI httpsURI = new URI("https://ya.ru/");
 			final URI httpURI = new URI("http://schemas.opengis.net/ogc_schema_updates.rss");
 			assertNotNull(resolver.getLastModified(httpsURI));
