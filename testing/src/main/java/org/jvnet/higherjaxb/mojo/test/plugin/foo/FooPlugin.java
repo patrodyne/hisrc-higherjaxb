eb2ed194cdadbb8c5b80e550d75d141a4f75dca3
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2023-06-28 13:33:50 -0400

    Improve FooPlugin design

--- testing/src/main/java/org/jvnet/higherjaxb/mojo/test/plugin/foo/FooPlugin.java
+++ testing/src/main/java/org/jvnet/higherjaxb/mojo/test/plugin/foo/FooPlugin.java
@@ -1,5 +1,7 @@
 package org.jvnet.higherjaxb.mojo.test.plugin.foo;
 
+import static java.lang.String.format;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -15,54 +17,77 @@ import com.sun.tools.xjc.model.CPropertyInfo;
 import com.sun.tools.xjc.outline.ClassOutline;
 import com.sun.tools.xjc.outline.FieldOutline;
 import com.sun.tools.xjc.outline.Outline;
+import com.sun.tools.xjc.util.ErrorReceiverFilter;
 
-public class FooPlugin extends Plugin {
-
+public class FooPlugin extends Plugin
+{
 	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	/** Represents the XJC plugin usage format.*/
+	public static final String USAGE_FORMAT = "  -%-20s : %s";
+
+	/** Name of Option to enable this plugin. */
+	private static final String OPTION_NAME = "Xfoo";
+	
+	/** Description of Option to enable this plugin. */
+	private static final String OPTION_DESC = "does nothing";
 
 	@Override
-	public String getOptionName() {
+	public String getOptionName()
+	{
 		return "Xfoo";
 	}
 
 	@Override
-	public String getUsage() {
-		return "  -Xfoo              :  does nothing";
+	public String getUsage()
+	{
+		return format(USAGE_FORMAT, OPTION_NAME, OPTION_DESC);
 	}
 
 	@Override
-	public boolean run(Outline outline, Options opt, ErrorHandler errorHandler) {
-		for (final ClassOutline classOutline : outline.getClasses()) {
-			for (final CPluginCustomization pluginCustomization : classOutline.target
-					.getCustomizations()) {
-				pluginCustomization.markAsAcknowledged();
-			}
-			final CClassInfo classInfo = classOutline.target;
-			logger.debug("Class:" + classInfo.getName());
-
-			for (final FieldOutline fieldOutline : classOutline
-					.getDeclaredFields()) {
-
-				final CPropertyInfo propertyInfo = fieldOutline
-						.getPropertyInfo();
-				logger.debug("Property:" + propertyInfo.getName(true));
-
-				for (final CPluginCustomization pluginCustomization : fieldOutline
-						.getPropertyInfo().getCustomizations()) {
-					pluginCustomization.markAsAcknowledged();
-				}
-			}
-		}
-		return true;
-	}
-
-	@Override
-	public List<String> getCustomizationURIs() {
+	public List<String> getCustomizationURIs()
+	{
 		return Collections.emptyList();
 	}
 
 	@Override
-	public boolean isCustomizationTagName(String nsUri, String localName) {
+	public boolean isCustomizationTagName(String nsUri, String localName)
+	{
 		return true;
+	}
+	
+	@Override
+	public boolean run(Outline outline, Options opt, ErrorHandler errorHandler)
+	{
+		for (final ClassOutline classOutline : outline.getClasses())
+		{
+			for (final CPluginCustomization pluginCustomization : classOutline.target.getCustomizations())
+				pluginCustomization.markAsAcknowledged();
+			
+			final CClassInfo classInfo = classOutline.target;
+			logger.debug("Class:" + classInfo.getName());
+			
+			for (final FieldOutline fieldOutline : classOutline.getDeclaredFields())
+			{
+				final CPropertyInfo propertyInfo = fieldOutline.getPropertyInfo();
+				logger.debug("Property:" + propertyInfo.getName(true));
+
+				for (final CPluginCustomization pluginCustomization : fieldOutline.getPropertyInfo().getCustomizations())
+					pluginCustomization.markAsAcknowledged();
+			}
+		}
+		
+		return hadError(outline.getErrorReceiver());
+	}
+	
+	private boolean hadError(ErrorHandler errorHandler)
+	{
+		boolean hadError = false;
+		if ( errorHandler instanceof ErrorReceiverFilter )
+		{
+			ErrorReceiverFilter errorReceiverFilter = (ErrorReceiverFilter) errorHandler;
+			hadError = errorReceiverFilter.hadError();
+		}
+		return hadError;
 	}
 }
